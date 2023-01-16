51bc2a4f10c6fca1d8258666e5996179e64cd668
Russell Laboe <russell-laboe@outlook.com>
2023-01-15 20:46:40 -0700

    fixed watcher script

--- watcher.sh
+++ watcher.sh
@@ -7,23 +7,28 @@ echo "listening for openapi.yaml changes..."
 while [[ true ]]
 do
     chsum2=`find pkg -type f -wholename 'pkg/*/api/openapi.yaml' -exec md5sum {} \;`
-    if [[ $chsum1 != $chsum2 ]] ; then
+    if [[ $chsum1 != $chsum2 ]]; then
         if [ -n "$chsum1" ]; then
-            IFS='/'
+            while IFS= read -r line; do
+                if ! [[ $chsum1 == *"$line"* ]]; then
+                    IFS='/'
+                    echo $line
 
-            #Read the split words into an array based on comma delimiter
-            read -a strarr <<< "$chsum1"
+                    #Read the split words into an array based on comma delimiter
+                    read -a strarr <<< "$line"
 
-            pkgName="${strarr[1]}"
-            echo "regenerating $pkgName code"
-            printf "\napi/openapi.yaml" >> "pkg/$pkgName/.openapi-generator-ignore"
-            sed -i '$a api/openapi.yaml' "pkg/$pkgName/.openapi-generator-ignore"
+                    pkgName="${strarr[1]}"
+                    echo "regenerating $pkgName code"
 
-            openapi-generator-cli generate -c codegencfg.json -i pkg/$pkgName/api/openapi.yaml --package-name $pkgName -o pkg/$pkgName --minimal-update >/dev/null && go fmt ./pkg/$pkgName/... >/dev/null && go fmt ./pkg/$pkgName/... >/dev/null
+                    sed -i '$a api/openapi.yaml' "pkg/$pkgName/.openapi-generator-ignore"
 
-            sed -i '$d' "pkg/$pkgName/.openapi-generator-ignore"
+                    openapi-generator-cli generate -c codegencfg.json -i pkg/$pkgName/api/openapi.yaml --package-name $pkgName -o pkg/$pkgName --minimal-update >/dev/null && go fmt ./pkg/$pkgName/... >/dev/null && go fmt ./pkg/$pkgName/... >/dev/null
 
-            echo "done"
+                    sed -i '$d' "pkg/$pkgName/.openapi-generator-ignore"
+
+                    echo "done"
+                fi
+            done <<< "$chsum2"
         fi
         chsum1=$chsum2
     fi
